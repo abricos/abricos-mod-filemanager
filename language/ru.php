c0cc39f1e8552626dac73f5bb24f7535ab7d2562
Alexander Kuzmin <roosit@abricos.org>
2009-10-05 11:25:52 +0000

    Импорт модулей

---
+++ language/ru.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* @version $Id: ru.php 10 2009-08-29 13:29:01Z roosit $
+* @package CMSBrick
+* @copyright Copyright (C) 2008 CMSBrick. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+*/
+
+return array(
+	'upload_unknowntype'=>'Неизвестный тип файла',
+	'upload_filesizelimit'=>'Размер файла превышает допустимый',
+	'upload_servererror'=>'Ошибка на стороне сервера',
+    'upload_piclimit'=>'Картинка превышает допустимый размер',
+    'upload_profilelimit'=>'В вашем профиле не хватает места для загрузки файла',
+    'upload_file'=>'Файл ',
+    'upload_notload'=> ' не загружен.',
+    'upload_loadok'=> ' успешно загружен на сайт.',
+    'upload_freespace'=> 'Свободного места в вашем профиле: ', 
+    'upload_close'=> 'Закрыть окно',
+    'upload_fileman'=> 'Управление файлами',
+    'upload_loadfile'=> 'Загрузить файл с вашего компьютера',
+    'upload_load'=> 'Загрузить',
+    'upload_key'=> 'Ключ вложения',
+    'upload_filetype'=> 'Тип файла',
+    'upload_maxfsize'=> 'Макс. размер файла',
+    'upload_maxpsize'=> 'Макс. размер картинки'
+
+);
+
+?>
